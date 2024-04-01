0d2fe7315b3aac0bec3ed23c2a3d0a3106bb3f4b
brater <93560561+braulio0643@users.noreply.github.com>
2024-03-31 22:52:59 -0300

    dale que se puede

--- tpfutbol.cpp
+++ tpfutbol.cpp
@@ -12,20 +12,66 @@ using namespace std;
 
 vector<tuple<int,int,string>> jugadores;
 tuple<int, int, vector<string>> maxActual(0, 0, {});
+vector<int> maxPosible(10,0);
+int maxDefensa = 0;
+int maxAtaque = 0;
 
-bool esMenor(vector<string> a, vector<string> b){
-    string stringMinimo = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
-    for(int j = 0; j < 5; j++){
-        if(a[j] < stringMinimo){
-            stringMinimo = a[j];
+
+void crearMaxPosible(vector<tuple<int,int,string>> &s, int n){
+    int i, j;
+    vector<tuple<int,int,string>> copia = s;
+
+    tuple<int,int,string> key;
+    for(i = 1; i < n; i++){
+        key = (copia[i]);
+        j = i-1;
+        while(j>= 0 && get<0>(s[j]) < get<0>(key)){
+            copia[j+1] = copia[j];
+            j= j-1;
         }
+        copia[j+1] = key;
     }
-    for(int j = 0; j < 5; j++){
-        if(b[j] < stringMinimo ){
-            return false;
+
+    int suma = 0;
+    
+    for(int i = 0; i < 5; i++){
+        suma += get<0>(copia[i]); 
+    }
+    maxAtaque = suma;
+    
+    for(i = 0; i < n; i++){
+        key = (copia[i]);
+        j = i-1;
+        while(j>= 0 && get<1>(copia[j]) < get<1>(key)){
+            copia[j+1] = copia[j];
+            j= j-1;
         }
+        copia[j+1] = key;
     }
-    return true;
+
+    suma = 0;
+    
+    for(int i = 0; i < 5; i++){
+        suma += get<1>(copia[i]); 
+    }
+
+    maxDefensa = suma;
+    
+}
+
+void sortNombres(vector<tuple<int,int,string>> &s, int n){
+
+    tuple<int,int,string> key;
+    int i, j;
+    for(i = 1; i < n; i++){
+        key = (s[i]);
+        j = i-1;
+        while(j>= 0 && get<2>(s[j]) > get<2>(key)){
+            s[j+1] = s[j];
+            j= j-1;
+        }
+        s[j+1] = key;
+    }
 }
 
 tuple<int,int, vector<string>> futbol(vector<string> &PI, int i, vector<int> &disponible, int sumaAtaque, int sumaDefensa){
@@ -35,8 +81,11 @@ tuple<int,int, vector<string>> futbol(vector<string> &PI, int i, vector<int> &di
     if(i==10){
         tuple<int,int,vector<string>> res(sumaAtaque, sumaDefensa, PI);
         return(res);
-    } else {
-        if(i>=5 && sumaAtaque < get<0>(maxActual)){
+    } else { //
+        if((i>=5 && sumaAtaque < get<0>(maxActual))
+        || (i < 5 && sumaAtaque + maxAtaque < get<0>(maxActual))
+        || (i >= 5 && sumaDefensa + maxDefensa < get<1>(maxActual))
+        ){
             return maxActual;
         }
         for(int j = 0; j < 10; j++){
@@ -45,15 +94,26 @@ tuple<int,int, vector<string>> futbol(vector<string> &PI, int i, vector<int> &di
                 PI.push_back(get<2>(jugadores[j]));
                 tuple<int,int, vector<string>> candidato;
                 if (i < 5){
+                    
                     candidato = futbol(PI, i+1, disponible, sumaAtaque + get<0>(jugadores[j]), sumaDefensa);
+                    
                 } else {
-                    candidato = futbol(PI, i+1, disponible, sumaAtaque, sumaDefensa + get<1>(jugadores[j]));
+                    vector<string> PICandidato = PI;
+                    int sumaDefensaCandidata = 0;
+                    for(int k = 0; k < 10; k++){
+                        if(disponible[k]== 1){
+                            PICandidato.push_back(get<2>(jugadores[k]));
+                            sumaDefensaCandidata+= get<1>(jugadores[k]);
+                        }
+                    } 
+                    tuple<int,int, vector<string>> resultado(sumaAtaque, sumaDefensaCandidata, PICandidato);
+                    candidato = resultado;
+                    
                 }
                 disponible[j] = 1;
                 PI.pop_back();
                 if((get<0>(candidato) > get<0>(maxActual))
                 || (get<0>(candidato)==get<0>(maxActual) && get<1>(candidato) > get<1>(maxActual))
-                || (get<0>(candidato)==get<0>(maxActual) && get<1>(candidato) == get<1>(candidato) && esMenor(get<2>(candidato), get<2>(maxActual)))
                 ){ //
                     maxActual = candidato;
                 }
@@ -63,34 +123,14 @@ tuple<int,int, vector<string>> futbol(vector<string> &PI, int i, vector<int> &di
     } 
 }
 
-void sort(vector<tuple<int,int,string>> &s, int n){
-    int i, j;
-    tuple<int,int,string> key;
-    for(i = 1; i < n; i++){
-        key = (s[i]);
-        j = i-1;
-        while(j>= 0 && get<0>(s[j]) < get<0>(key)){
-            s[j+1] = s[j];
-            j= j-1;
-        }
-        s[j+1] = key;
-    }
-    for(i = 6; i < n; i++){
-        key = (s[i]);
-        j = i-1;
-        while(j>= 0 && get<1>(s[j]) < get<1>(key)){
-            s[j+1] = s[j];
-            j= j-1;
-        }
-        s[j+1] = key;
-    }
-
-}
-
 int main(){
     int n;
     cin >> n;
-    while(n > 0){
+    int caso = 1;
+    while(caso <= n){
+        get<0>(maxActual) = 0;
+        get<1>(maxActual) = 0;
+        get<2>(maxActual) = {};
         for(int i = 0; i < 10; i++){
             tuple<int,int,string> jugador;
             string nombre;
@@ -104,9 +144,11 @@ int main(){
             jugadores.push_back(jugador);
         }
         
-        sort(jugadores, 10);
+        crearMaxPosible(jugadores, 10);
+        sortNombres(jugadores, 10);
         vector<int> disponible(10,1);
         vector<string> PI = {};
+
         tuple<int,int, vector<string>> res = futbol(PI, 0, disponible, 0, 0);
         PI = get<2>(res);
 
@@ -120,12 +162,13 @@ int main(){
                 defensores.push_back(PI[i]);
             }
         }
-        
+
         sort(atacantes.begin(), atacantes.end());
         sort(defensores.begin(), defensores.end());
         jugadores = {};
 
-        cout << "Case " << n << ":" << endl;
+
+        cout << "Case " << caso << ":" << endl;
         cout << "(";
         for(int i = 0; i < 4; i++){
             cout << atacantes[i] << ", ";
@@ -136,6 +179,7 @@ int main(){
             cout << defensores[i] << ", ";
         }
         cout << defensores[4] << ")" << endl;
-        n--;
+        caso++;
     }
+    
 }
